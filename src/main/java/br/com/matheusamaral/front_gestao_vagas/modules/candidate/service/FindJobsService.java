af35ccd88afc3f204fc6bde0de9086c5bef58209
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-10-25 12:56:27 -0300

    feat: Uso do ParameterizedTypeReference

--- src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/FindJobsService.java
+++ src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/FindJobsService.java
@@ -1,7 +1,9 @@
 package br.com.matheusamaral.front_gestao_vagas.modules.candidate.service;
 
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -10,12 +12,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.matheusamaral.front_gestao_vagas.modules.candidate.dto.JobDTO;
+
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FindJobsService {
 
-    public String execute(String token, String filter) {
+    public List<JobDTO> execute(String token, String filter) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
@@ -25,9 +30,10 @@ public class FindJobsService {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/candidate/job")
                 .queryParam("filter", filter);
 
+        ParameterizedTypeReference<List<JobDTO>> responseType = new ParameterizedTypeReference<List<JobDTO>>(){};
+
         try {
-            var result = rt.exchange(builder.toUriString(), HttpMethod.GET, request, String.class);
-            System.out.println(result);
+            var result = rt.exchange(builder.toUriString(), HttpMethod.GET, request, responseType);
             return result.getBody();
         } catch (Unauthorized ex) {
             throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
