703d02cef4804ff30e6b35cf46ddb527fa674060
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-12-08 17:30:35 -0300

    refactor: Função list()

--- src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/company/CompanyController.java
+++ src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/company/CompanyController.java
@@ -19,6 +19,7 @@ import br.com.matheusamaral.front_gestao_vagas.modules.company.dto.CreateCompany
 import br.com.matheusamaral.front_gestao_vagas.modules.company.dto.CreateJobsDTO;
 import br.com.matheusamaral.front_gestao_vagas.modules.company.service.CreateCompanyService;
 import br.com.matheusamaral.front_gestao_vagas.modules.company.service.CreateJobService;
+import br.com.matheusamaral.front_gestao_vagas.modules.company.service.ListAllJobsCompanyService;
 import br.com.matheusamaral.front_gestao_vagas.modules.company.service.LoginCompanyService;
 import br.com.matheusamaral.front_gestao_vagas.utils.FormatErrorMessage;
 import jakarta.servlet.http.HttpSession;
@@ -35,6 +36,9 @@ public class CompanyController {
 
     @Autowired
     private CreateJobService createJobService;
+
+    @Autowired
+    private ListAllJobsCompanyService listAllJobsCompanyService;
    
     @GetMapping("/create")
     public String create(Model model){
@@ -102,6 +106,8 @@ public class CompanyController {
     @GetMapping("/jobs/list")
     @PreAuthorize("hasRole('COMPANY')")
     public String list(Model model){
+        var result = this.listAllJobsCompanyService.execute(getToken());
+        model.addAttribute("jobs", result);
         return "company/list";
     }
 
