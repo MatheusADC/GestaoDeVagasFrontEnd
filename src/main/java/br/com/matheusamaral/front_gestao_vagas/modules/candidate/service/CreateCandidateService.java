563be5eff2f31ac1d09ae431a0d18abe13835a9d
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-12-11 12:04:29 -0300

    refactor: hostAPIGestaoVagas em CreateCandidateService

--- src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/CreateCandidateService.java
+++ src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/CreateCandidateService.java
@@ -1,5 +1,6 @@
 package br.com.matheusamaral.front_gestao_vagas.modules.candidate.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -11,6 +12,9 @@ import br.com.matheusamaral.front_gestao_vagas.modules.candidate.dto.CreateCandi
 @Service
 public class CreateCandidateService {
     
+    @Value("${host.api.gestao.vagas}")
+    private String hostAPIGestaoVagas;
+
     public void execute(CreateCandidateDTO createCandidateDTO) {
         
             RestTemplate rt = new RestTemplate();
@@ -20,6 +24,8 @@ public class CreateCandidateService {
 
             HttpEntity<CreateCandidateDTO> request = new HttpEntity<>(createCandidateDTO ,headers);
 
-            var result = rt.postForObject("http://localhost:8080/candidate/", request, String.class);
+            var url = hostAPIGestaoVagas.concat("/candidate/");
+
+            var result = rt.postForObject(url, request, String.class);
     }
 }
