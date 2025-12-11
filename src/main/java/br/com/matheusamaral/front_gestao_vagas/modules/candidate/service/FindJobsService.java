9e3b1ff52ec486cfa349e16a071193ccad72bde2
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-12-11 12:04:06 -0300

    refactor: hostAPIGestaoVagas em FindJobsService

--- src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/FindJobsService.java
+++ src/main/java/br/com/matheusamaral/front_gestao_vagas/modules/candidate/service/FindJobsService.java
@@ -3,6 +3,7 @@ package br.com.matheusamaral.front_gestao_vagas.modules.candidate.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -20,6 +21,9 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class FindJobsService {
 
+    @Value("${host.api.gestao.vagas}")
+    private String hostAPIGestaoVagas;
+
     public List<JobDTO> execute(String token, String filter) {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
@@ -27,7 +31,9 @@ public class FindJobsService {
 
         HttpEntity<Map<String, String>> request = new HttpEntity<>(headers);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost:8080/candidate/job")
+        var url = hostAPIGestaoVagas.concat("/candidate/job");
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
                 .queryParam("filter", filter);
 
         ParameterizedTypeReference<List<JobDTO>> responseType = new ParameterizedTypeReference<List<JobDTO>>(){};
